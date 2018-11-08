8cfc66874a563861324311d4a4bd4820b160cf6d
Alena <alena1999simonova@yandex.ru>
2018-11-08 15:19:01 +0300

    Initial commit

---
+++ src/main/java/example/alenushkasim/aircraft_catalog/AircraftCatalogApplication.java
@@ -0,0 +1,12 @@
+package example.alenushkasim.aircraft_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AircraftCatalogApplication.class, args);
+    }
+}
